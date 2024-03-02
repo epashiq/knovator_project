93939e536e729d2e1cdd60d32516b2ff58e7afda
epashiq <ashiqashi4715@gmail.com>
2024-03-02 13:24:27 +0530

    Resume Page shifted stateless widget to consumer widget

--- lib/view/pages/resume_page.dart
+++ lib/view/pages/resume_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:knovator_project/controller/resume_provider.dart';
+import 'package:knovator_project/view/pages/resume_view_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class ResumePage extends StatelessWidget {
+class ResumePage extends ConsumerWidget {
   final ValueNotifier<bool> themeNoitfier;
   const ResumePage({super.key, required this.themeNoitfier});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     void changeTheme() async {
       themeNoitfier.value = !themeNoitfier.value;
       (await SharedPreferences.getInstance())
@@ -15,6 +18,7 @@ class ResumePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text('RESUME'),
         actions: [
           IconButton(
@@ -22,6 +26,16 @@ class ResumePage extends StatelessWidget {
               icon: Icon(
                   themeNoitfier.value ? Icons.light_mode : Icons.dark_mode))
         ],
+      ),
+      body: ListView.builder(
+        itemCount: ref.watch(resumeProvider).length,
+        itemBuilder: (context, index) {
+          final resume = ref.watch(resumeProvider)[index];
+          return ListTile(
+            onTap: () => const ViewResumePAge(),
+            title: Text(resume.name),
+          );
+        },
       ),
       bottomNavigationBar: TextButton(
         style: TextButton.styleFrom(
---
+++ lib/view/pages/resume_view_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ViewResumePAge extends StatelessWidget {
+  const ViewResumePAge({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
